cc37b12f4c91a6efcbf7510cff0bc084ac18b442
Riyadh Noori <alexea.iskinder@gmail.com>
2026-03-11 22:15:23 +0300

    fix: disable firmware-chroot to skip Contents download

--- build-system/scripts/wget-wrapper.sh
+++ build-system/scripts/wget-wrapper.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 # wget wrapper - silently handles Contents-amd64.gz 404 errors
 REAL_WGET=/usr/bin/wget
-ARGS=("$@")
-for arg in "${ARGS[@]}"; do
+for arg in "$@"; do
   if [[ "$arg" == *"Contents"* ]]; then
-    for i in "${!ARGS[@]}"; do
-      if [[ "${ARGS[$i]}" == "-O" ]]; then
-        OUTFILE="${ARGS[$((i+1))]}"
-        if [ -n "$OUTFILE" ]; then
-          printf '\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x03\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00' > "$OUTFILE"
-        fi
-        exit 0
-      fi
-    done
     exit 0
   fi
 done
