cea50075303adb6324ec93440b105e8a05de8072
Riyadh Noori <alexea.iskinder@gmail.com>
2026-03-13 00:55:34 +0300

    fix: keep lb_binary_grub2 intact for boot sector

--- build-system/scripts/wget-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# wget wrapper - silently handles Contents-amd64.gz 404 errors
-REAL_WGET=/usr/bin/wget
-for arg in "$@"; do
-  if [[ "$arg" == *"Contents"* ]]; then
-    exit 0
-  fi
-done
-exec "$REAL_WGET" "$@"
